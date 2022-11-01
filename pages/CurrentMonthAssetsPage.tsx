f49f76acc7bffe2f7f4af66962d0414def575436
Michael Spagnolo <mspagnolo27@gmail.com>
2022-11-01 12:22:39 -0400

    Checkpoint Before Context changed to Map

--- pages/CurrentMonthAssetsPage.tsx
+++ pages/CurrentMonthAssetsPage.tsx
@@ -1,18 +1,12 @@
 import {
   AssetMapping,
-  ExpenseDetail,
-  PrismaClient,
   Receipts,
 } from "@prisma/client";
 import type { NextPage } from "next";
 import {
   Fragment,
-  MouseEventHandler,
   useContext,
-  useEffect,
-  useState,
 } from "react";
-import { json } from "stream/consumers";
 import DataContext from "../components/context/data-context";
 import ItemTable from "../components/ItemTable/ItemTable";
 import { Item } from "../containerModel/Models/Item";
@@ -31,30 +25,27 @@ const CurrentMonthAssetsPage: NextPage = () => {
       });
   };
 
-  const updateReceiptsHandler = async (e: { preventDefault: any; }) => {
-
-    console.log("ex")
+  const updateReceiptsHandler = async (e: { preventDefault: any }) => {
+    console.log("ex");
     e.preventDefault;
     const receipts = await fetch(`/api/receipts`)
       .then((response) => response.json())
-      .then((data) =>  data);
+      .then((data) => data);
 
     const updateItems = appContext.items.map((item, idx) => {
       let receipt: Receipts = receipts.find(
         (receipt: Receipts) => receipt.GlAccount == item.GlAccount
       );
 
-      let update:{[key:string]:number} = {
-        ReceiptQty: 0+item.ReceiptQty,
-        ReceiptValue: 0+item.ReceiptValue,
+      let update: { [key: string]: number } = {
+        ReceiptQty: 0 + item.ReceiptQty,
+        ReceiptValue: 0 + item.ReceiptValue,
       };
-     
 
       if (receipt) {
-
-         update = {
-          ReceiptQty: receipt.QtyClean||0 + item.ReceiptQty,
-          ReceiptValue: receipt.ReceiptValue||0+ item.ReceiptValue,
+        update = {
+          ReceiptQty: receipt.QtyClean || 0 + item.ReceiptQty,
+          ReceiptValue: receipt.ReceiptValue || 0 + item.ReceiptValue,
         };
       }
 
@@ -64,7 +55,7 @@ const CurrentMonthAssetsPage: NextPage = () => {
     appContext.updateItems(updateItems);
   };
 
-  const initalizeAssetMapHandler = async (e: { preventDefault: any; }) => {
+  const initalizeAssetMapHandler = async (e: { preventDefault: any }) => {
     e.preventDefault;
     const assetMapping = await fetch(`/api/assetMapping`)
       .then((response) => response.json())
@@ -98,5 +89,3 @@ const CurrentMonthAssetsPage: NextPage = () => {
 };
 
 export default CurrentMonthAssetsPage;
-
-
